d774501cc6a56ca5d613bc207536d78a09008dda
KylieM <kyliethedev@gmail.com>
2021-08-03 14:46:08 +0900

    Lambda

---
+++ Lambda.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace CSharpStudy
+{
+    delegate int Calculate(int a, int b);
+    delegate string Concatenate(string[] args);
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,30 @@ namespace CSharpStudy
     {
         static void Main(string[] args)
         {
-            DelegateClass mc = new DelegateClass();
-            mc.Perform();
+            // 13. Delegate
+            // DelegateClass mc = new DelegateClass();
+            // mc.Perform();
+
+            // 14. Lambda
+            // Calculate cal = (x, y) => x + y;
+            // Console.WriteLine("Lambda cal : " + cal(10, 20));        
+
+            // Concatenate concat = ( arr ) => {
+            //                                     string result = "";
+            //                                     foreach (string s in arr) 
+            //                                         result += s;
+
+            //                                     return result;
+            //                                 };
+            // Console.WriteLine("Lambda concat : " + concat(args));
+
+            Func<int, int> func = (x) => x * 2;
+            Console.WriteLine("Lambda func : " + func(4));
+
+            int actionResult = 0;
+            Action<int> action = (x) => actionResult = x * 10;
+            action(10);
+            Console.WriteLine("Lambda action : " + actionResult);
         }
     }
 }
